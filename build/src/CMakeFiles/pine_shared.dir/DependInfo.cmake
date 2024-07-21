caee5687728e68f3ff7564f08b21e3f87e3d464d
wanzihui <2632641494@qq.com>
2024-07-21 18:49:03 +0800

    Second commit

---
+++ build/src/CMakeFiles/pine_shared.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wanzihui/C++Server/day16/src/Acceptor.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Acceptor.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/Buffer.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Buffer.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/Channel.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Channel.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/Connection.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Connection.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/EventLoop.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/EventLoop.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/Poller.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Poller.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/Socket.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/Socket.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/TcpServer.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/TcpServer.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/ThreadPool.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/ThreadPool.cpp.o"
+  "/home/wanzihui/C++Server/day16/src/util.cpp" "/home/wanzihui/C++Server/day16/build/src/CMakeFiles/pine_shared.dir/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../test/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/echo_clients.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo_clients.dir/echo_clients.cpp.o"
+  "../bin/echo_clients.pdb"
+  "../bin/echo_clients"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/echo_clients.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
